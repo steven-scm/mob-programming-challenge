2cef1b8adaf3fc63ab30bdb22120ee3f3915e36a
Emilie Martin <emilie.martindonati@gmail.com>
2022-01-07 12:20:56 +0100

    it 6

--- app.js
+++ app.js
@@ -11,28 +11,9 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
 
-const users = [
-    {
-        name: "Arthur",
-        email:"arth@foo.com",
-        favoriteLanguage: "javascript"
-    },
-    {
-        name: "Alexandra",
-        email:"alex@foo.com",
-        favoriteLanguage: "javascript"
-    },
-    {
-        name: "Steven",
-        email:"steven@foo.com",
-        favoriteLanguage: "javascript"
-    },
-];
 
-const images = [
-    "/images/christopher-gower-m_HRfLhgABo-unsplash.jpg",
-    "/images/florian-olivo-4hbJ-eymZ1o-unsplash.jpg",
-    "/images/joshua-reddekopp-SyYmXSDnJ54-unsplash.jpg"    
-];
+// Added require
+
+app.use('/', require("./routes/routesIndex"));
 
 app.listen(process.env.PORT, () => console.log(`listening to port ${process.env.PORT}`));
---
+++ routes/routesIndex.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = new express.Router();
+
+const users = [
+    {
+        name: "Arthur",
+        email:"arth@foo.com",
+        favoriteLanguage: "javascript"
+    },
+    {
+        name: "Alexandra",
+        email:"alex@foo.com",
+        favoriteLanguage: "javascript"
+    },
+    {
+        name: "Steven",
+        email:"steven@foo.com",
+        favoriteLanguage: "javascript"
+    },
+];
+
+const images = [
+    "/images/christopher-gower-m_HRfLhgABo-unsplash.jpg",
+    "/images/florian-olivo-4hbJ-eymZ1o-unsplash.jpg",
+    "/images/joshua-reddekopp-SyYmXSDnJ54-unsplash.jpg"    
+];
+
+router.get('/', (req, res, next) => {
+    res.render("../views/home.hbs", {images});
+})
+
+router.get('/my-dev-squad', (req, res, next) => {
+    res.render("../views/my-dev-squad.hbs", 
+    {users});
+})
+
+router.get('/add-new-ironhacker', (req, res, next) => {
+    res.render("../views/add-new-ironhacker.hbs");
+})
+
+module.exports = router;
