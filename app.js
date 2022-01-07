77757c0253ba498a033777264e2488332465c5ce
Hernan Menajovsky <hmenajovsky@gmail.com>
2022-01-07 11:59:16 +0100

    Solved iteration 5

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("Hello, World")
+require("dotenv").config();
+require("./config/mongodb.js");
+
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+//app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+const users = [
+    {
+        name: "Arthur",
+        email:"arth@foo.com",
+        favoriteLanguage: "javascript"
+    },
+    {
+        name: "Alexandra",
+        email:"alex@foo.com",
+        favoriteLanguage: "javascript"
+    },
+    {
+        name: "Steven",
+        email:"steven@foo.com",
+        favoriteLanguage: "javascript"
+    },
+];
+
+const images = [
+    "/images/christopher-gower-m_HRfLhgABo-unsplash.jpg",
+    "/images/florian-olivo-4hbJ-eymZ1o-unsplash.jpg",
+    "/images/joshua-reddekopp-SyYmXSDnJ54-unsplash.jpg"    
+];
+
+app.listen(process.env.PORT, () => console.log(`listening to port ${process.env.PORT}`));
--- config/mongodb.js
+++ config/mongodb.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost/my-dev-squad", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
